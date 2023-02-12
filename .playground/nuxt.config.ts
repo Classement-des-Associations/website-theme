19a0b8169abd7b5bfe9ef45d7072ac1a835805a3
Barbapapazes <e.soubiran25@gmail.com>
2023-02-12 11:05:29 +0100

    initial commit

---
+++ .playground/nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+  debug: true,
+  extends: '..',
+  typescript: {
+    includeWorkspace: true
+  }
+})
---
+++ .playground/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <BaseSection>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae harum tempore reiciendis eum maiores inventore est possimus. Sed ipsam aut maiores. Reprehenderit omnis mollitia quia deserunt eum inventore, quos beatae?
+  </BaseSection>
+</template>
