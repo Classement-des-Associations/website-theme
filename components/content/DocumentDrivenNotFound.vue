eb73d4216aadda9112a5ce53f3116f1c28d1cbb7
Barbapapazes <e.soubiran25@gmail.com>
2023-02-27 14:14:54 +0100

    feat: add document driven not found

---
+++ components/content/DocumentDrivenNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseSection class="py-32 flex flex-col items-center">
+    <h1 class="text-center text-transparent bg-clip-text bg-classement font-bold text-5xl">
+      Vous cherchez votre chemin ?
+    </h1>
+    <p class="mt-16 text-center text-xl text-black">
+      Nous n'avons pas trouvé cette page. <br>
+      Heureusement nous pouvons vous ramener sur la page d'accueil.
+    </p>
+    <div class="mt-8 flex flex-col md:flex-row justify-center gap-8">
+      <ClassementLink button-link="/" button-text="Accueil du Classement" part="classement" />
+      <NewsletterLink button-text="Newsletter" part="classement" />
+    </div>
+  </BaseSection>
+</template>
