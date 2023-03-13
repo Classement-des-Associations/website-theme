5359c2af9f248cfa3215cfb5ecc5f84ef78e28aa
Barbapapazes <e.soubiran25@gmail.com>
2023-03-13 20:03:10 +0100

    feat: add a grid component fix #84

---
+++ components/base/BaseGrid.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  is?: string;
+  disableTwoColumns?: boolean;
+}>()
+</script>
+
+<template>
+  <component
+    :is="is ?? 'ul'"
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-x-8 lg:gap-y-12"
+    :class="{ 'md:grid-cols-2': !disableTwoColumns, 'md:grid-cols-3': disableTwoColumns }"
+  >
+    <slot />
+  </component>
+</template>
